f27fb1a49535d8648419b2b463693483f20cc3f5
maryamrmz <maryam.rpour@gmail.com>
2023-05-23 00:41:50 +0330

    test: write test for component

---
+++ components/Card/Card.test.tsx
@@ -0,0 +1,14 @@
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+
+import Card from './index';
+
+test(`should render a card with a chidren`, () => {
+  const component = render(
+    <Card>
+      <span>I am a child!</span>
+    </Card>
+  );
+
+  expect(component).toMatchSnapshot();
+});
