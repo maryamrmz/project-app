a379f474504d2aac34614aac14ed58e0a3684a0b
maryamrmz <maryam.rpour@gmail.com>
2023-05-22 14:56:11 +0330

    feat: add different type of button

--- components/Button/Button.stories.tsx
+++ components/Button/Button.stories.tsx
@@ -2,9 +2,48 @@ import Button from './index';
 
 import type { Meta, StoryObj } from '@storybook/react';
 
-const meta: Meta<typeof Button> = { component: Button };
+const meta: Meta<typeof Button> = {
+  component: Button,
+  args: {
+    children: 'Button',
+  },
+};
 export default meta;
 
 type Story = StoryObj<typeof Button>;
 
-export const Primary: Story = { args: { children: 'Button' } };
+export const Primary: Story = {
+  args: {
+    intent: 'primary',
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    intent: 'secondary',
+  },
+};
+
+export const Text: Story = {
+  args: {
+    intent: 'text',
+  },
+};
+
+export const Large: Story = {
+  args: {
+    size: 'large',
+  },
+};
+
+export const Medium: Story = {
+  args: {
+    size: 'medium',
+  },
+};
+
+export const Small: Story = {
+  args: {
+    size: 'small',
+  },
+};
