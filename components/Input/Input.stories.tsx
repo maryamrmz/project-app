f3bd4ba3e69ef93541b8971713a2161e51e15537
maryamrmz <maryam.rpour@gmail.com>
2023-05-22 15:31:49 +0330

    feat: add Input story

---
+++ components/Input/Input.stories.tsx
@@ -0,0 +1,43 @@
+import { Meta, StoryObj } from '@storybook/react';
+import Input from './index';
+
+const meta: Meta<typeof Input> = {
+  component: Input,
+  args: {
+    placeholder: 'Input',
+    onChange: () => ({}),
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Input>;
+
+export const Required: Story = {
+  args: {
+    type: 'text',
+    required: false,
+    value: 'input',
+  },
+};
+
+export const Email: Story = {
+  args: {
+    type: 'email',
+    value: 'input@gmail.com',
+  },
+};
+
+export const Number: Story = {
+  args: {
+    type: 'number',
+    value: '1234567',
+  },
+};
+
+export const Password: Story = {
+  args: {
+    type: 'password',
+    value: 'input',
+  },
+};
--- components/Input/index.tsx
+++ components/Input/index.tsx
@@ -13,6 +13,7 @@ interface InputProps {
 const Input: FC<InputProps> = ({ className, type = 'text', ...props }) => {
   return (
     <input
+      type={type}
       className={clsx(
         'border-solid border-gray border-2 px-6 py-2 text-lg rounded-3xl w-full',
         className
